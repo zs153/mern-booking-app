6d3b1fa5be2bfecb30b4516bbebbe81030c9b842
zs53 <tmp153@gmail.com>
2024-11-06 21:29:15 +0100

    añadido pruebas crear hotel

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -8,8 +8,8 @@ test("should allow the user to sign in", async ({ page }) => {
   // get de sign in button
   await page.getByRole("link", { name: "Sign In" }).click();
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
-  await page.locator("[name=email]").fill("1@1.com");
-  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=email]").fill("borrar.me@bizkaia.eus");
+  await page.locator("[name=password]").fill("123456");
   await page.getByRole("button", { name: "Login" }).click();
   await expect(page.getByText("Sign in successful!")).toBeVisible();
   await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
